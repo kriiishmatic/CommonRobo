17684f84a7ecae49dd502a110d88c8aa01e82fd6
kriiishamtic <kriiishmatic@gmail.com>
2025-09-27 14:20:12 +0530

    user

---
+++ common.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#colours
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Get sudo access BOZO $N "
+    exit 3
+fi
+
+#creating logs files
+####################
+
+shell_log="/var/log/robo-project"
+
+mkdir -p $shell_log
+
+#removing .sh from file
+#######################
+
+Remove_sh=$( echo $0 | cut -d "." -f1 )
+
+Logfile="$shell_log/$Remove_sh.log"
+##Time taken ####
+echo " Script started at :: $(date) " | tee -a $Logfile
+Start=$( date +%s )
+
+Status(){
+    if [ $1 -ne 0 ]; then
+    echo -e " $R Failed $N  $2  " | tee -a $Logfile
+    exit 1
+else
+    echo -e "$G Sucessfully $N $2 " | tee -a $Logfile
+fi
+}
+
+##### MONGODBINSTALLSTION #########
+mongo_installation(){
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+Status $? "created repo "
+
+dnf install mongodb-org -y &>>$Logfile
+Status $? "Installed mongodb::sure"
+
+systemctl enable mongod &>>$Logfile
+
+systemctl start mongod &>>$Logfile
+Status $? "Start Mongod Hurray!!"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+Status $? "Allowing remote connections: kek "
+
+systemctl restart mongod &>>$Logfile
+Status $? " Mongod restarted and ready to go "
+
+}
+
+
+TIMER(){
+
+End=$( date +%s )
+Time=$(( $Start - $End ))
+echo -e " Time Taken to setup ::: $G $Time $N "
+
+}
