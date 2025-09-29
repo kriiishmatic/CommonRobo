a539b67dfafce830c9d6921129d7480d6515c427
kriiishamtic <kriiishmatic@gmail.com>
2025-09-29 11:23:49 +0530

    Your commit message

--- Instance_records.sh
+++ Instance_records.sh
@@ -1,41 +1,54 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="ami-09c813fb71547fc4f" # replace with your SG ID
-ZONE_ID="Z0212707MY585LOOEFGA" # replace with your ID
+SG_ID="sg-05e5bd14656ebdaf3"   # replace with your actual Security Group ID
+ZONE_ID="Z0212707MY585LOOEFGA" # replace with your Hosted Zone ID
 DOMAIN_NAME="kriiishmatic.fun"
 
-for instance in $@ # mongodb redis mysql
+# Loop through instances passed as arguments (e.g., mongodb redis mysql frontend)
+for instance in "$@"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    # Launch instance
+    INSTANCE_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-    # Get Private IP
-    if [ $instance != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.kriiishmatic.fun
+    # Get IP (private for backend, public for frontend)
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+            --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # kriiishmatic.fun
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
+            --output text)
+        RECORD_NAME="$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
 
+    # Update DNS record in Route53
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Updating record set"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$RECORD_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP'"
+        --hosted-zone-id "$ZONE_ID" \
+        --change-batch "{
+            \"Comment\": \"Updating record set\",
+            \"Changes\": [{
+                \"Action\": \"UPSERT\",
+                \"ResourceRecordSet\": {
+                    \"Name\": \"$RECORD_NAME\",
+                    \"Type\": \"A\",
+                    \"TTL\": 1,
+                    \"ResourceRecords\": [{
+                        \"Value\": \"$IP\"
+                    }]
+                }
             }]
-        }
-        }]
-    }
-    '
+        }"
 done
